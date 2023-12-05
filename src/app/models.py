1115fccf95c3ac5184587c16c53963144b7be7ca
Evgeniya <polianskaia.ea@phystech.edu>
2023-12-05 18:24:12 +0300

    feature: make migrations

--- src/app/models.py
+++ src/app/models.py
@@ -2,7 +2,7 @@ from tortoise import models, fields
 from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
 
 
-class User(models.Model):
+class Users(models.Model):
     id = fields.IntField(pk=True)
     email = fields.CharField(max_length=100, unique=True)
     hashed_password = fields.CharField(max_length=1000)
@@ -15,6 +15,6 @@ class User(models.Model):
     class PydanticMeta:
         exclude = ['hashed_password']
 
-UserPydantic = pydantic_model_creator(User, name="User")
-UserInPydantic = pydantic_model_creator(User, name='User', exclude_readonly=True)
-UserPydanticList = pydantic_queryset_creator(User)
+UserPydantic = pydantic_model_creator(Users, name="User")
+UserInPydantic = pydantic_model_creator(Users, name='User', exclude_readonly=True)
+UserPydanticList = pydantic_queryset_creator(Users)
--- src/main.py
+++ src/main.py
@@ -4,31 +4,32 @@ from fastapi import FastAPI
 
 from tortoise.contrib.fastapi import register_tortoise, HTTPNotFoundError
 
-from app.models import UserPydantic, UserInPydantic, User
+from app.models import UserPydantic, UserInPydantic, Users
+from config import TORTOISE_ORM, db_url
 
 app = FastAPI()
 
 
 @app.get("/users", response_model=List[UserPydantic])
 async def get_users():
-    return await UserInPydantic.from_queryset(User.all())
+    return await UserInPydantic.from_queryset(Users.all())
 
 
 @app.get("/users/{user_id}", response_model=UserPydantic, responses={404: {'model': HTTPNotFoundError}})
 async def get_user(user_id: int):
-    return await UserPydantic.from_queryset_single(User.get(id=user_id))
+    return await UserPydantic.from_queryset_single(Users.get(id=user_id))
 
 
 @app.post("/users", response_model=UserPydantic)
 async def create_user(user: UserInPydantic):
-    user_obj = await User.create(**user.dict(exclude_unset=True))
+    user_obj = await Users.create(**user.dict(exclude_unset=True))
     return await UserPydantic.from_tortoise_orm(user_obj)
 
 
 register_tortoise(
     app,
-    db_url="sqlite://:memory:",
-    modules={"models": ["app.models"]},
+    db_url=db_url,
+    modules={"models": TORTOISE_ORM["apps"]["models"]["models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
