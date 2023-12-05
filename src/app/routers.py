bc868748e4a78cb97aaae9ce86fccc24c8fd9c29
Evgeniya <polianskaia.ea@phystech.edu>
2023-12-05 18:52:45 +0300

    feature: added routers

---
+++ src/app/routers.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from fastapi import APIRouter
+from tortoise.contrib.fastapi import HTTPNotFoundError
+
+from .models import UserInPydantic, UserPydantic, Users
+
+router = APIRouter()
+
+@router.get("/users", response_model=List[UserPydantic])
+async def get_users():
+    return await UserInPydantic.from_queryset(Users.all())
+
+
+@router.get("/users/{user_id}", response_model=UserPydantic, responses={404: {'model': HTTPNotFoundError}})
+async def get_user(user_id: int):
+    return await UserPydantic.from_queryset_single(Users.get(id=user_id))
+
+
+@router.post("/users", response_model=UserPydantic)
+async def create_user(user: UserInPydantic):
+    user_obj = await Users.create(**user.dict(exclude_unset=True))
+    return await UserPydantic.from_tortoise_orm(user_obj)
--- src/main.py
+++ src/main.py
@@ -7,23 +7,13 @@ from tortoise.contrib.fastapi import register_tortoise, HTTPNotFoundError
 from app.models import UserPydantic, UserInPydantic, Users
 from config import TORTOISE_ORM, db_url
 
+from app.routers import router as app_router
+
 app = FastAPI()
 
 
-@app.get("/users", response_model=List[UserPydantic])
-async def get_users():
-    return await UserInPydantic.from_queryset(Users.all())
+app.include_router(app_router, prefix="/api/v1", tags=['users'])
 
-
-@app.get("/users/{user_id}", response_model=UserPydantic, responses={404: {'model': HTTPNotFoundError}})
-async def get_user(user_id: int):
-    return await UserPydantic.from_queryset_single(Users.get(id=user_id))
-
-
-@app.post("/users", response_model=UserPydantic)
-async def create_user(user: UserInPydantic):
-    user_obj = await Users.create(**user.dict(exclude_unset=True))
-    return await UserPydantic.from_tortoise_orm(user_obj)
 
 
 register_tortoise(
