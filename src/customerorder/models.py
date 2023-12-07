074a57e2a2c6e6c204f013071342ceee73451e76
Evgeniya <polianskaia.ea@phystech.edu>
2023-12-07 14:52:36 +0300

    feature: added new columns to customerorder table

--- src/customerorder/models.py
+++ src/customerorder/models.py
@@ -4,9 +4,16 @@ from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_
 
 class CustomerOrders(models.Model):
     id = fields.UUIDField(pk=True)
+    id_ms = fields.CharField(max_length=100)
     code = fields.CharField(max_length=100, unique=True)
-    date = fields.DatetimeField()
-    products = fields.JSONField()
+    created = fields.DatetimeField()
+    positions = fields.JSONField()
+    delivery_date = fields.DatetimeField()
+    state = fields.CharField(max_length=100)
+    update_date = fields.DatetimeField()
+    store = fields.CharField(max_length=100)
+    sum = fields.FloatField()
+    agent = fields.CharField(max_length=100)
 
 
 CustomerOrderPydantic = pydantic_model_creator(CustomerOrders, name="CustomerOrder")
